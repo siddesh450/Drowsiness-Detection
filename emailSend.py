68b210981e15c817dc99812635bf4490bc355672
Siddesh Somani <86416013+siddesh450@users.noreply.github.com>
2024-04-13 02:44:24 +0530

    Add files via upload

---
+++ emailSend.py
@@ -0,0 +1,54 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def emailSender():
+    sender_email = "nu582149@gmail.com"
+    receiver_email = "g464048@gmail.com"
+    password = "Naruto@#123&$"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Driver Detected in Drowsiness State"
+    message["From"] = "Veer Solanki"
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    # text = """\
+    # Hi,
+    # How are you?
+    # Real Python has many great tutorials:
+    # www.realpython.com"""
+    html = """\
+    <html>
+    <body>
+        <p><br>
+            <h2>Driver Details:</h2>
+            <h5>Name: Ajinkya Solanke </h5>
+            <h5>Mobile No: 1234567890 </h5>
+            <h5>Vehicle Type: Car </h5>
+        </p>
+    </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    # part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    # message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com",465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+    print('Email Send Sucessfully')
+
+if __name__ == "__main__":   
+    emailSender()
